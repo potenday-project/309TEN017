430199db8820eeb6e8bebb9026d2d31b1700571a
minai621 <minai621@naver.com>
2023-09-17 19:16:55 +0900

    supabase init and bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
-import './App.css';
-import ProviderWrapper from './ProviderWrapper';
+import { Button } from '@nextui-org/react';
+import { useState } from 'react';
+import ProviderWrapper from './components/wrapper/ProviderWrapper';
+import supabase from './supabaseClinet';
 
 function App() {
+  const [text, setText] = useState('');
+
+  const saveText = async () => {
+    const testData = {
+      test: text,
+    };
+    const { data, error } = await supabase
+      .from('Test')
+      .insert([testData])
+      .select();
+
+    if (error) {
+      console.error('Error saving text:', error);
+    } else {
+      alert('Text saved successfully!');
+      setText('');
+    }
+    console.log('data is: ', data);
+  };
+
   return (
     <div className='App'>
       <ProviderWrapper>
-        <div className='text-[50px]'>App</div>
+        <input
+          type='text'
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder='Enter your text here...'
+        />
+        <Button onClick={saveText}>Save Text</Button>
       </ProviderWrapper>
     </div>
   );
