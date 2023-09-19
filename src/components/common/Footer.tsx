c726839630ff503e1878ffc8b8d7401da0f80601
Yang Seok Yeong <seokyeong.yang@kakao.com>
2023-09-20 05:59:26 +0900

    chore: add Header, Footer Components

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <footer className="absolute bottom-0 w-full h-[90px] bg-red-300">Footer Component</footer>;
+}
---
+++ src/components/common/Header.tsx
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <footer className="h-[90px] bg-red-300">Footer Comp</footer>;
+}
