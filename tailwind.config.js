09cae4ffee2f8b0f39655cd366f0abab831cb191
Yang Seok Yeong <seokyeong.yang@kakao.com>
2023-09-20 05:22:53 +0900

    chore: apply webfont

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
 const { nextui } = require('@nextui-org/react');
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Pretendard', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   darkMode: 'class',
   plugins: [nextui()],
