3b72b64ef6fd599787ee8e2c3b0d5bab4eb2e685
minai621 <minai621@naver.com>
2023-09-20 14:15:55 +0900

    feat: user, board, comment model

---
+++ src/types/db/BoardComment.ts
@@ -0,0 +1,8 @@
+type BoardComment = {
+  id: number;
+  created_at: Date;
+  content: string;
+  board_id: string;
+};
+
+export default BoardComment;
---
+++ src/types/db/User.ts
@@ -0,0 +1,10 @@
+type Users = {
+  id: number;
+  created_at: Date;
+  name: string;
+  password: string;
+  save_money: number;
+  level: number;
+};
+
+export default Users;
