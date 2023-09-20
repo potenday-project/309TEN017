4229d51c8d294578e5762324551dec4c796d5956
Yang Seok Yeong <seokyeong.yang@kakao.com>
2023-09-21 06:47:18 +0900

    chore: apply Link partially

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,10 +1,14 @@
+import { Link } from 'react-router-dom';
+
 export default function Footer() {
   return (
     <footer className="absolute bottom-0 w-full h-[90px] bg-red-300">
-      <div className="flex justify-between h-full">
-        <button type="button" className="bg-red-200">
-          털털홈
-        </button>
+      <div className="flex justify-between h-full [&_a]:h-full [&_button]:h-full">
+        <Link to="/">
+          <button type="button" className="bg-red-200">
+            털털홈
+          </button>
+        </Link>
 
         <button type="button" className="bg-red-200">
           글쓰기
