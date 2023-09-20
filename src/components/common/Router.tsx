89df798f39829b4a677545eb11d64587fdf344a2
Yang Seok Yeong <seokyeong.yang@kakao.com>
2023-09-21 04:24:10 +0900

    refactor: move position

--- src/components/common/Router.tsx
+++ src/components/common/Router.tsx
@@ -1,12 +1,10 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from '../../pages/Home';
 
 export default function Router() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
