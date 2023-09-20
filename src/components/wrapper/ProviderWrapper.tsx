c2a03ad2e508b2a38276035c6355a97a2dc9f5e1
Yang Seok Yeong <seokyeong.yang@kakao.com>
2023-09-21 03:43:53 +0900

    refactor: detach react-router

--- src/components/wrapper/ProviderWrapper.tsx
+++ src/components/wrapper/ProviderWrapper.tsx
@@ -1,17 +1,12 @@
 import { NextUIProvider } from '@nextui-org/react';
 import { ReactNode } from 'react';
-import { BrowserRouter } from 'react-router-dom';
 
 interface ProviderWrapperProps {
   children: ReactNode;
 }
 
 function ProviderWrapper({ children }: ProviderWrapperProps) {
-  return (
-    <BrowserRouter>
-      <NextUIProvider>{children}</NextUIProvider>
-    </BrowserRouter>
-  );
+  return <NextUIProvider>{children}</NextUIProvider>;
 }
 
 export default ProviderWrapper;
