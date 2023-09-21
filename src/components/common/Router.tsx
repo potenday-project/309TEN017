e8bcd259104965519692f6689cff422262eab57e
Yang Seok Yeong <seokyeong.yang@kakao.com>
2023-09-22 04:36:35 +0900

    remove: Login page component

--- src/components/common/Router.tsx
+++ src/components/common/Router.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../../pages/Home';
-import Login from '../../pages/Login';
 import Post from '../../pages/Post';
 import Guide from '../../pages/Guide';
 
@@ -8,7 +7,6 @@ export default function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
       <Route path="/post" element={<Post />} />
       <Route path="/guide" element={<Guide />} />
     </Routes>
