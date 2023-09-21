8250a9f27026fd32835636905d81ef35ce1c39b8
MJ <80272444+minai621@users.noreply.github.com>
2023-09-22 03:00:38 +0900

    fix: remove duplicated BrowserRouter. already in wrapper

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,6 @@
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+root.render(<App />);
