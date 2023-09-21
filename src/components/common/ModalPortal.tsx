487ebf09980b0781049a350ed606bc47ab619d2c
MJ <80272444+minai621@users.noreply.github.com>
2023-09-22 03:09:08 +0900

    update: add onOpenChange props to modal

--- src/components/common/ModalPortal.tsx
+++ src/components/common/ModalPortal.tsx
@@ -1,4 +1,4 @@
-import { Modal, ModalContent, ModalFooter } from '@nextui-org/react';
+import { Modal } from '@nextui-org/react';
 import ReactDOM from 'react-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { closeModal } from '../../store/modal/modalSlice';
@@ -8,29 +8,12 @@ function ModalPortal() {
   const dispatch = useDispatch();
   const Component = useSelector((state: RootState) => state.modal.component);
   const isOpen = useSelector((state: RootState) => state.modal.isOpen);
-  console.log(isOpen);
 
   if (!Component) return null;
 
   return ReactDOM.createPortal(
-    <Modal isOpen={isOpen} placement="center">
-      <ModalContent>
-        {onClose => (
-          <>
-            {Component}
-            <ModalFooter>
-              <button
-                onClick={() => {
-                  dispatch(closeModal());
-                  onClose();
-                }}
-              >
-                Close Modal
-              </button>
-            </ModalFooter>
-          </>
-        )}
-      </ModalContent>
+    <Modal isOpen={isOpen} placement="center" onOpenChange={() => dispatch(closeModal())}>
+      {Component}
     </Modal>,
     document.getElementById('root')!
   );
