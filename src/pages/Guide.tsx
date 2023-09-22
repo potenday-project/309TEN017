fb31f3144ae434103b25ac7e6fae7e98c221191e
Yang Seok Yeong <seokyeong.yang@kakao.com>
2023-09-22 21:29:26 +0900

    design: contents aligned

--- src/pages/Guide.tsx
+++ src/pages/Guide.tsx
@@ -1,9 +1,9 @@
 export default function Guide() {
   return (
     <div>
-      <h3 className="my-[30px]">거지 월드가 처음이시라면 이렇게 즐겨보세요!</h3>
+      <h3 className="flex justify-center my-[30px] font-bold text-xl">거지 월드가 처음이시라면 이렇게 즐겨보세요!</h3>
 
-      <img src="/guide.png" alt="Guide" />
+      <img src="/guide.png" alt="Guide" className="mx-auto" />
     </div>
   );
 }
