60360de18b837d599b37875fb44da26409fef935
MJ <80272444+minai621@users.noreply.github.com>
2023-09-23 04:14:47 +0000

    update: use JIT for tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const { nextui } = require('@nextui-org/react');
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  mode: 'jit',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
