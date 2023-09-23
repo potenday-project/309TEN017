d32c3bc90b7dca3420e26637925b8639196bf003
MJ <80272444+minai621@users.noreply.github.com>
2023-09-23 04:21:37 +0000

    layout: apply navbar for Header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,19 +1,18 @@
+import { Divider, Navbar, NavbarBrand, NavbarContent } from '@nextui-org/react';
 import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header className="h-[90px] bg-red-300">
-      <div className="flex justify-between h-full">
-        <h1 className="bg-red-200">LOGO AREA</h1>
-
-        <h2 className="bg-red-200 [&>a]:inline-block [&>a]:h-full">
+    <header className="h-[90px]">
+      <Navbar className="relative">
+        <NavbarBrand className="absolute top-[24px]">
+          <h1 className="bg-red-200">LOGO</h1>
+        </NavbarBrand>
+        <NavbarContent className="flex !justify-center text-[32px] font-bold">
           <Link to="/">거지 월드</Link>
-        </h2>
-
-        <button type="button" className="bg-red-200">
-          도움말
-        </button>
-      </div>
+        </NavbarContent>
+      </Navbar>
+      <Divider />
     </header>
   );
 }
