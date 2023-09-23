991f7d33a93aa066c747c10af98f79b65d724efe
MJ <80272444+minai621@users.noreply.github.com>
2023-09-23 05:27:06 +0000

    update: apply logo

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -5,8 +5,12 @@ export default function Header() {
   return (
     <header className="h-[90px]">
       <Navbar className="relative">
-        <NavbarBrand className="absolute top-[24px]">
-          <h1 className="bg-red-200">LOGO</h1>
+        <NavbarBrand className="absolute top-[24px] flex items-center">
+          <img
+            src={`${process.env.PUBLIC_URL}/logo.png`}
+            className="w-[30px] h-[30px] object-cover"
+            alt="logo"
+          />
         </NavbarBrand>
         <NavbarContent className="flex !justify-center text-[32px] font-bold">
           <Link to="/">거지 월드</Link>
